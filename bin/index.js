00c924922590bd7bcb10eb1c4a6c963cb8aa0f19
Shahid Shaikh <shahid@codeforgeek.com>
2015-06-16 16:58:20 +0530

    Bootstrapping

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+var express	=	require("express");
+var app		=	express();
+
+app.get('/',function(req,res){
+	res.send("Hello");
+});
+
+app.listen(3000,function(){
+	console.log("I am running at 3000");
+});
